497d6a23fd2c5992659e554feb3d90b3bc16eec9
William Stephens <william.a.stephens17@gmail.com>
2020-09-01 13:42:13 -0500

    XML Comments

--- TrackerLibrary/MatchupModel.cs
+++ TrackerLibrary/MatchupModel.cs
@@ -5,8 +5,17 @@ namespace TrackerLibrary
 {
     public class MatchupModel
     {
+        /// <summary>
+        /// Represents the Entries for thi
+        /// </summary>
         public List<MatchupEntryModel> Entries { get; set; }
+
+        /// <summary>
+        /// Represents the winner of the matchup.
+        /// </summary>
         public TeamModel Winner { get; set; }
+
+
         public int MatchupRound { get; set; }
     }
 }
