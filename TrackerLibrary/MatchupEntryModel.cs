57358ad6f6fc11c66725f408ed2182c2ee570fed
William Stephens <william.a.stephens17@gmail.com>
2020-08-31 19:07:07 -0500

    Finished class library

---
+++ TrackerLibrary/MatchupEntryModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace TrackerLibrary
+{
+    public class MatchupEntryModel
+    {
+        public TeamModel TeamCompeting { get; set; }
+        public double Score { get; set; }
+        public MatchupModel ParentMatchup { get; set; }
+    }
+}
--- TrackerLibrary/TrournamentModel.cs
+++ TrackerLibrary/TrournamentModel.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace TrackerLibrary
 {
     public class TrournamentModel
     {
-        public TrournamentModel()
-        {
-        }
+        public string TrournamentName { get; set; }
+        public decimal EntryFee { get; set; }
+        public List<TeamModel> EnteredTeams { get; set; } = new List<TeamModel>();
+        public List<PrizeModel> Prizes { get; set; } = new List<PrizeModel>();
+        public List<List<MatchupModel>> Rounds { get; set; } = new List<List<MatchupModel>>();
     }
 }
